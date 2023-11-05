d893bf7968ba80c4ef06180bbf1770f839216ac1
M-Samuel <samuel.m.modeste@gmail.com>
2023-11-05 14:19:47 +0000

    added labels for errors

--- Angular-Learn/src/app/product-creation/product-creation.component.ts
+++ Angular-Learn/src/app/product-creation/product-creation.component.ts
@@ -1,5 +1,5 @@
-import { Component, inject } from '@angular/core';
-import { FormBuilder, Validators } from '@angular/forms';
+import { Component, OnInit, inject } from '@angular/core';
+import { AbstractControl, FormBuilder, FormControl, ValidationErrors, Validators } from '@angular/forms';
 import { ProductService } from '../product/product.service';
 import { Product } from '../types/product';
 
@@ -8,26 +8,47 @@ import { Product } from '../types/product';
   templateUrl: './product-creation.component.html',
   styleUrls: ['./product-creation.component.scss']
 })
-export class ProductCreationComponent {
+export class ProductCreationComponent{
+  hasSubmitted = false;
   private readonly _formBuilder = inject(FormBuilder)
   private readonly _productService = inject(ProductService)
 
   readonly createProductFG = this._formBuilder.group({
-    name: [null, [Validators.required, Validators.maxLength(20)]],
-    image: [null, [Validators.required]],
+    name: new FormControl<string|null>(null,{validators: [Validators.required, Validators.maxLength(20)], updateOn: 'submit'}),
+    image: new FormControl<string|null>(null,{validators: [Validators.required, simpleUrlValidator], updateOn: 'submit'})
   })
 
   readonly createdProduct$ = this._productService.createdProduct$
-  
+
 
   onSubmit(){
-    
+    this.hasSubmitted = true
     if(this.createProductFG.valid){
       const product: Product = {} as Product
       Object.assign(product, this.createProductFG.value)
       console.log(product)
       this._productService.emitEvent({action:'CreateNewProduct',value:product})
     }
-
+    else{
+      console.log(this.createProductFG)
+    }
   }
+
+  get nameControl(){
+    return this.createProductFG.controls['name']
+  }
+
+  get imageControl(){
+    return this.createProductFG.controls['image']
+  }
+}
+
+
+function simpleUrlValidator(control:AbstractControl): ValidationErrors | null{
+  const value:string = control.value
+  if(!value) return null
+
+  const validationError: ValidationErrors | null= !value.includes('http') ? {invalidUrlFormat:true} : null
+
+  return validationError
 }
