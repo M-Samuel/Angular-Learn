4788059349647edb9d27e47784655fc05df37ea8
M-Samuel <samuel.m.modeste@gmail.com>
2023-11-05 08:08:07 +0000

    using events to link products actions

--- Angular-Learn/src/app/product/product.component.ts
+++ Angular-Learn/src/app/product/product.component.ts
@@ -1,17 +1,26 @@
-import { Component, inject } from '@angular/core';
+import { Component, OnInit, inject } from '@angular/core';
 import { ProductService } from './product.service';
+import { Product } from '../types/product';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-product',
   templateUrl: './product.component.html',
   styleUrls: ['./product.component.scss']
 })
-export class ProductComponent {
+export class ProductComponent implements OnInit{
   private readonly _productService:ProductService = inject(ProductService)
-
-  products$ = this._productService.products$()
+  
+  readonly products$: Observable<Product[]> = this._productService.products$
+  readonly createdProduct$: Observable<Product> = this._productService.createdProduct$
+  
+  
+  ngOnInit(): void {
+    this._productService.emitEvent({action: 'RequestAllProducts', value: undefined})
+  }
 
   createProduct(){
-    this._productService.createProduct({name:"Smarties",image:""})
+    const newProduct: Product = {name:"Smarties",image:""}
+    this._productService.emitEvent({action:'CreateNewProduct', value: newProduct})
   }
 }
--- Angular-Learn/src/app/product/product.service.ts
+++ Angular-Learn/src/app/product/product.service.ts
@@ -1,5 +1,5 @@
 import { Injectable, inject } from '@angular/core';
-import { Observable } from 'rxjs';
+import { BehaviorSubject, Observable, OperatorFunction, Subject, filter, map, switchMap, tap } from 'rxjs';
 import { Product } from '../types/product';
 import { DataStoreService } from '../DataStore/data-store.service';
 
@@ -9,13 +9,57 @@ import { DataStoreService } from '../DataStore/data-store.service';
 export class ProductService {
 
   private readonly _httpClient: DataStoreService = inject(DataStoreService)
-  constructor() { }
+  constructor() {
+    this._stateEvent$.subscribe(stateEvent => {
+      const newState = this.dispatchEvent(stateEvent, this._stateSubject$.value)
+      this._stateSubject$.next(newState)
+    })
+  }
+  
 
-  products$(): Observable<Product[]>{
-    return this._httpClient.httpGetAllProducts()
+  private readonly _stateEventSubject$ = new Subject<StateEvent>()
+  private readonly _stateSubject$ = new BehaviorSubject<State>({} as State)
+  private readonly _stateEvent$ = this._stateEventSubject$.asObservable()
+  private readonly _state$ = this._stateSubject$.asObservable().pipe(
+    filter(state => !!state.lastEvent)
+  )
+
+  emitEvent(stateEvent: StateEvent){
+    console.log(stateEvent, `Emitted Event`)
+    this._stateEventSubject$.next(stateEvent)
   }
 
-  createProduct(product:Product): Observable<Product>{
-    return this._httpClient.httpPostProduct(product)
+  dispatchEvent(stateEvent: StateEvent, oldState: State): State{
+    let newState:State = {...oldState, lastEvent: stateEvent}
+    if(stateEvent.action === 'CreateNewProduct')
+      newState = {...newState, newProduct: stateEvent.value}
+    return newState
   }
+
+
+  readonly products$: Observable<Product[]> = this._state$.pipe(
+    filter(state => state.lastEvent.action === 'RequestAllProducts'),
+    switchMap(state => this._httpClient.httpGetAllProducts())
+  )
+
+  readonly createdProduct$ = this._state$.pipe(
+    filter(state => state.lastEvent.action === 'CreateNewProduct'),
+    filter(state => !!state.newProduct),
+    map(state => state.newProduct) as OperatorFunction<State,Product>,
+    switchMap(product => this._httpClient.httpPostProduct(product)),
+    tap(product => this.emitEvent({action: 'RequestAllProducts', value: undefined}))
+  )
+}
+
+export interface StateEvent{
+  value: any,
+  action: 
+  'PageLoad'|
+  'CreateNewProduct'|
+  'RequestAllProducts'
+}
+
+interface State{
+  lastEvent: StateEvent,
+  newProduct?: Product
 }
